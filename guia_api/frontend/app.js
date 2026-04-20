3c31d58b99a79c07b4b381186ecbb5f98edf7382
Alvaro Andres Salazar Herrera <salazares2005@gmail.com>
2026-04-20 18:24:58 -0500

    Delete guia_api/frontend/app.js

--- guia_api/frontend/app.js
+++
@@ -1,9 +0,0 @@
-import { ejercicio4 } from './Ejercicio4/index.js';
-
-let mensajeReporte = prompt("Ingrese la ID a eliminar");
-
-// Envio el ID a eliminar
-const respuesta = await ejercicio4(mensajeReporte);
-
-console.log(respuesta);
-
