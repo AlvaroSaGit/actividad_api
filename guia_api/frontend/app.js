d280c1ef6fcadac290e12287b4242e3f13144924
Alvaro <salazares2005@gmail.com>
2026-04-20 17:24:51 -0500

    Feat: Se hizo el enunciado numero 4

--- guia_api/frontend/app.js
+++ guia_api/frontend/app.js
@@ -1,2 +1,8 @@
-import { get, crear, actualizar, eliminar } from './util/index.js';
+import { ejercicio4 } from './Ejercicio4/index.js';
 
+let mensajeReporte = prompt("Ingrese la ID a eliminar");
+
+// Envio el ID a eliminar
+const respuesta = await ejercicio4(mensajeReporte);
+
+console.log(respuesta);
